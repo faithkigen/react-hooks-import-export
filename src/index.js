cc078c08b1c1802cd714251931664acd6e68bbaa
Faith <kigenfaith20@gmail.com>
2023-01-15 00:28:19 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ColoradoStateParks from "./ColoradoStateParks";
-import Hogwarts from "./Hogwarts";
+
 
 ReactDOM.render(<ColoradoStateParks />, document.getElementById("root"));
